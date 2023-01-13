3dcbf55470b11e7cdabeb2276600e201209af83b
SurvePranav <pranavsurve009@gmail.com>
2023-01-13 22:59:16 +0530

    test for auth provioder

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:makenote/services/auth/auth_service.dart';
 import 'package:makenote/views/notes_view.dart';
 import 'package:makenote/views/register_view.dart';
 import 'package:makenote/views/verify_email_view.dart';
-import 'views/login_view.dart';
+import 'package:makenote/views/login_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 class AuthUser implements Exception {
   final bool isEmailVerified;
 
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  const AuthUser({required this.isEmailVerified});
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
